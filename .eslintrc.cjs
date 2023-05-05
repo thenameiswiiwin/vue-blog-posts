fb06b35d2957c547ccd30c9a85c2a6d10ff97d7a
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-05-04 20:50:43 -0500

    chore: configs prettier, husky, eslint, commitlint, stylelint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,23 +3,69 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true
+  },
+  plugins: ['vue'],
+  parser: 'vue-eslint-parser',
   overrides: [
     {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', '*.vue'],
+      parser: 'vue-eslint-parser',
+      extends: ['plugin:cypress/recommended']
     }
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: [
+    'eslint-config-airbnb-base',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+    'no-var': 'error',
+    'no-new': 1,
+    'no-shadow': 0,
+    'no-console': 0,
+    'no-underscore-dangle': 0,
+    'no-confusing-arrow': 0,
+    'no-plusplus': 0,
+    'no-param-reassign': 0,
+    'no-restricted-syntax': 0,
+    'no-use-before-define': 0,
+    'no-prototype-builtins': 0,
+    'no-unneeded-ternary': 'error',
+    'no-duplicate-imports': 'error',
+    'no-useless-computed-key': 'error',
+    'no-useless-escape': 0,
+    'no-continue': 0,
+    indent: ['error', 2, { SwitchCase: 1 }],
+    camelcase: 0,
+    'class-methods-use-this': 0,
+    'new-cap': 0,
+    'func-style': 0,
+    'max-len': 0,
+    'consistent-return': 0,
+    'default-case': 2,
+    'rest-spread-spacing': 'error',
+    'prefer-const': 'error',
+    'arrow-spacing': 'error',
+    'prefer-destructuring': ['error', { object: true, array: false }]
   }
 }
---
+++ stylelint.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  root: true,
+  defaultSeverity: 'error',
+  plugins: ['stylelint-order'],
+  extends: [
+    'stylelint-config-tailwindcss',
+    'stylelint-config-standard', // the standard shareable config for Stylelint
+    'stylelint-config-html/html', // the shareable html config for Stylelint.
+    'stylelint-config-html/vue', // the shareable vue config for Stylelint.
+    'stylelint-config-recess-order' // use the clean order for properties
+  ],
+  rules: {
+    'no-descending-specificity': null,
+    'no-empty-source': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+          'function',
+          'if',
+          'each',
+          'include',
+          'mixin'
+        ]
+      }
+    ],
+    'function-no-unknown': null,
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    'declaration-property-value-no-unknown': true,
+    'selector-no-vendor-prefix': null,
+    'keyframes-name-pattern': null,
+    'selector-class-pattern': null,
+    'value-no-vendor-prefix': null,
+    'rule-empty-line-before': ['always', { ignore: ['after-comment', 'first-nested'] }]
+  },
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+  overrides: [
+    {
+      files: ['*.vue', '**/*.vue', '*.html', '**/*.html'],
+      customSyntax: 'postcss-html',
+      rules: {
+        'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['deep', 'global'] }],
+        'selector-pseudo-element-no-unknown': [
+          true,
+          { ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted'] }
+        ]
+      }
+    }
+  ]
+}
