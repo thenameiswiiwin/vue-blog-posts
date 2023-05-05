8dbe4dec92bf4df146f339970f9198f6edcc02c5
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-05-04 22:04:59 -0500

    feat(TimeLine.vue): made period tabs reactive using ref api, and full typesafety

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
-const periods = ['Today', 'This Week', 'This Month']
+import { ref } from 'vue'
+
+const periods = ['Today', 'This Week', 'This Month'] as const
+
+type Period = (typeof periods)[number]
+
+const selectedPeriod = ref<Period>('Today')
+
+function isActive(period: Period) {
+  return selectedPeriod.value === period
+}
+
+function selectPeriod(period: Period) {
+  selectedPeriod.value = period
+}
 </script>
 
 <template>
@@ -8,11 +22,12 @@ const periods = ['Today', 'This Week', 'This Month']
       v-for="period in periods"
       :key="period"
       :class="[
-        period === 'Today'
+        isActive(period)
           ? 'border-green-500 text-green-600'
           : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
         'w-1/4 border-b-2 px-1 py-4 text-center text-sm font-medium'
       ]"
+      @click="selectPeriod(period)"
     >
       {{ period }}
     </a>
