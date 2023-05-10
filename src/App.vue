3f5016cf0b0385ec4e2a3f5673dfb44242c6e194
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-05-10 16:41:42 -0500

    feat: setup vue router, and move content from App.vue to HomeView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
-<script setup lang="ts">
-import TimeLine from '@/components/TimeLine.vue'
-import LoadingSpinner from '@/components/LoadingSpinner.vue'
-</script>
-
 <template>
   <div class="overflow-hidden rounded-lg bg-white">
-    <Suspense>
-      <template #fallback>
-        <TimeLine />
-      </template>
-
-      <template #default>
-        <LoadingSpinner />
-      </template>
-    </Suspense>
+    <RouterView />
   </div>
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TimeLine from '@/components/TimeLine.vue'
+import LoadingSpinner from '@/components/LoadingSpinner.vue'
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <TimeLine />
+    </template>
+
+    <template #fallback>
+      <LoadingSpinner />
+    </template>
+  </Suspense>
+</template>
