245544ac168b2ef21be6a3122b598f4b716838fb
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-05-24 13:34:45 -0500

    feat(App.vue, FormInput.vue): created FormInput component and implement a two-way binding with v-model

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,22 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import NavBar from '@/components/NavBar.vue'
 import Modal from '@/components/Modal.vue'
+import FormInput from '@/components/FormInput.vue'
+
+const username = ref('')
 </script>
 
 <template>
   <Modal />
   <div class="overflow-hidden rounded-lg bg-white">
     <div class="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="pt-4">
+        <FormInput name="Username" v-model="username" />
+      </div>
       <NavBar />
       <RouterView />
+      {{ username }}
     </div>
   </div>
 </template>
-
-<style>
-@import 'highlight.js/styles/tokyo-night-dark.css';
-
-ul {
-  list-style: revert !important;
-  list-style-type: inside !important;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  margin: 10px 0 !important;
-  font-size: revert !important;
-}
-
-pre {
-  margin: 10px 0 !important;
-}
-
-p {
-  margin: 10px 0 !important;
-}
-</style>
---
+++ src/components/FormInput.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+defineProps<{
+  name: string
+  modelValue: string
+}>()
+
+const emit = defineEmits<{
+  (event: 'update:modelValue', value: string): void
+}>()
+
+function handleInput(e: Event) {
+  const { value } = e.target as HTMLInputElement
+  emit('update:modelValue', value)
+}
+</script>
+
+<template>
+  <div class="relative">
+    <label
+      :for="name"
+      class="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+      >{{ name }}</label
+    >
+    <input
+      type="text"
+      name="name"
+      :id="name"
+      :value="modelValue"
+      @input="handleInput"
+      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+    />
+  </div>
+</template>
