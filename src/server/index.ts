b54a03ef0e56bb06ceb5b7ec293605e1ba9a4dae
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-05-24 16:35:43 -0500

    feat(server/index.ts): singing a jwt

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,15 @@
+import type { Request, Response } from 'express'
 import * as express from 'express'
 import * as cors from 'cors'
 import * as bodyParser from 'body-parser'
+import * as cookieParser from 'cookie-parser'
+import * as jsonwebtoken from 'jsonwebtoken'
 import { thisMonth, thisWeek, today, type Post } from '../utils/posts'
 import type { NewUser, User } from '../utils/users'
 
 const app = express()
 app.use(cors())
+app.use(cookieParser())
 app.use(bodyParser.json())
 
 const allPosts = [today, thisWeek, thisMonth]
@@ -21,9 +25,21 @@ app.post<{}, {}, Post>('/posts', (req, res) => {
   res.json(post)
 })
 
+const TOKEN = 'secret-token'
+const COOKIE = 'cookie-jwt'
+
+function authenicate(id: string, req: Request, res: Response) {
+  const token = jsonwebtoken.sign({ id }, TOKEN, {
+    issuer: 'thenameiswiiwin',
+    expiresIn: '30 days'
+  })
+  res.cookie(COOKIE, token, { httpOnly: true })
+}
+
 app.post<{}, {}, NewUser>('/users', (req, res) => {
   const user: User = { ...req.body, id: (Math.random() * 100000).toFixed() }
   allUsers.push(user)
+  authenicate(user.id, req, res)
   const { password, ...rest } = user
   res.json(rest)
 })
